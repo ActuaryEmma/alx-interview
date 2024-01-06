f5bacb4ee5cb8fa4e717bc216f1cbb77d9dc2b36
ActuaryEmma <emmanyakio@gmail.com>
2024-01-06 21:31:18 +0300

    pycode style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,6 +15,6 @@ def pascal_triangle(n):
         row = [1] * (i + 1)
         triangle.append(row)
     for i in range(n):
-        for j in range ( 1, i ):
+        for j in range (1, i):
             triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
     return triangle
