879aa84e70041bc493a41b6cb2124ea647a9ea43
ActuaryEmma <emmanyakio@gmail.com>
2024-02-03 14:52:21 +0300

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,9 +9,11 @@ def validUTF8(data):
     return True if the data is a valid UTF-8 encoding else return false
     """
     try:
-        bytes(data).decode('utf-8', "strict")
+        for byte in data:
+            # Ensure that only the 8 least significant bits are considered
+            byte = byte & 0xFF
+            byte_data = bytes([byte])
+            byte_data.decode('utf-8', "strict")
         return True
     except UnicodeDecodeError:
         return False
-
-
