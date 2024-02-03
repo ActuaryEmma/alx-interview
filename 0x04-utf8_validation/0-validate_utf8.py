c2d7ae3eaed2d5ea06096e18459bb921a13464b0
ActuaryEmma <emmanyakio@gmail.com>
2024-02-03 14:33:36 +0300

    Task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+method that determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    return True if the data is a valid UTF-8 encoding else return false
+    """
+    try:
+        bytes(data).decode('utf-8', "strict")
+        return True
+    except UnicodeDecodeError:
+        return False
+
+
