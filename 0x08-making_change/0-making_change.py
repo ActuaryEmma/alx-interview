e4e923950f6a7265e0095e259178bda7627cf8f0
ActuaryEmma <emmanyakio@gmail.com>
2024-03-04 03:20:16 +0300

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet a given amount total.
+"""
+
+
 def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet a given amount total.
+    """
     if total <= 0:
         return 0
     dp = [float('inf')] * (total + 1)
